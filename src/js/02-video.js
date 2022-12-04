c93a081a9e17fb7e36e0cf12dbe1957de13581ae
Serg-Babak <babakserg1@gmail.com>
2022-12-04 22:09:10 +0200

    video add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const getCurrentPlayTime = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', throttle(getCurrentPlayTime, 1000));
+
+const getSecFromStorage = () => {
+  return JSON.parse(localStorage.getItem('videoplayer-current-time'));
+};
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(getSecFromStorage()).then(function (seconds) {
+    console.log(`Last time you stopped video on ${seconds} seconds`);
+  });
+}
